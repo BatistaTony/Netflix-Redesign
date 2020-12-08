60a23d886603d901eb97e9b88847836639c3d6c4
BatistaTony <batistatomeoliveira96@gmail.com>
2020-12-09 00:32:59 +0100

    just initiate the repos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a50ddd114475ad659305";
+/******/ 	var hotCurrentHash = "5c2b96fbdeb79b48cb7b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
